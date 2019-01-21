a6d512bdf9b88a19cb50429beeb02d1408ef5cb8
Eric <elymberopoulos@luc.edu>
2019-01-21 13:24:02 -0600

    console user commands now working from scanner user input. Switch statement for different commands.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,12 +3,32 @@ package main;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a command");
-        System.out.println("Valid commands are: ");
-        String commandInput = scanner.nextLine().toLowerCase();
-        System.out.println(commandInput);
+    public static void main(String[] args) {
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter a command");
+            System.out.println("Valid commands are: exit, add device, dim device, turn on, turn off");
+            String commandInput = scanner.nextLine().toLowerCase();
+            System.out.println("The command input is: " + commandInput);
+            switch(commandInput) {
+                case "add device" :
+                    System.out.println("ADD DEVICE TRIGGERED");
+                    break; // optional
 
+                case "exit" :
+                    // Statements
+                    break; // optional
+
+                // You can have any number of case statements.
+                default : // Optional
+                    // Statements
+            }
+            if (commandInput.contentEquals("exit")) {
+                break;
+            }
+
+
+        }while(true);
+        System.exit(0);
     }
 }
